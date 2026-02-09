f8158705471be9908a6d25c0adcb8888649e2286
Octave Graf <ocgraf@student.42.fr>
2026-02-09 18:03:13 +0100

    started to do some commands

--- include/commands.hpp
+++ include/commands.hpp
@@ -5,16 +5,23 @@
 #include "errors.hpp"
 #include "message.hpp"
 #include "Server.hpp"
+#include "User.hpp"
+
+class Server;
 
 // Utils
-User *searchUser(std::string nickname, std::vector<User &> users);
+User * searchUser(std::string nickname, std::vector<User *> users);
+int dispatchCommand(t_msg *msg, Server &server);
 
 // Server Scope
-int cap(t_msg *msg);
-int pass(t_msg *msg, const Server &server);
-int user(t_msg *msg, const Server &server);
+int cap(t_msg *msg, Server &server);
+int pass(t_msg *msg, Server &server);
+int user(t_msg *msg, Server &server, int sfd);
+int privmsg(t_msg *msg, Server &server);
+int pingpong(t_msg *msg, Server &server);
 
 // Channel Scope
+int mode(t_msg *msg, Server &server);
 
 // User Scope
-int nick(t_msg *msg, const Server &server);
+int nick(t_msg *msg, Server &server);
